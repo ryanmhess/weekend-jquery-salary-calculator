6e2db5cf22f5792877dc6e7001cd179a27f8049d
Ryan Hess <ryanmhess@gmail.com>
2022-08-29 09:05:58 -0500

    added comments to script.js file

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,13 @@ let annualSalaryTotal;
 
 $(document).ready(readyNow);
 
-//----------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------//
+//      READY NOW Function
+
+/*
+When the document is ready, this works as the main function hub.  The employeeList function auto
+pulls the employees array. There are also listeners for the add-btn, remove-btn, and the delete-btn
+*/
 
 function readyNow() {
     employeeList(employees);
@@ -17,6 +23,15 @@ function readyNow() {
     $('#remove-btn').on('click', employeeAdd);
     $(document).on('click', '.delete-btn', employeeDelete)
 }   //  end readyNow function
+
+//-----------------------------------------------------------------------------------------------//
+//      EMPLOYEE LIST Function
+
+/*
+This function clears the table body and sets the annualSalaryTotal to 0. Then a for loop is
+initiated cycling through each employee in the employees array and sends them through the
+employeeAssign and employeeSalaryTotal for further manipulation.
+*/
 
 function employeeList(){
     $('#tableBody').empty();
@@ -26,6 +41,14 @@ function employeeList(){
         employeeSalaryTotal(employee);
     }
 }   //  end employeeList function
+
+//-----------------------------------------------------------------------------------------------//
+//      EMPLOYEE ASSIGN Function
+
+/*
+This function adds employee data to the table. It also assigns the empID to the id of the delete
+button created for each employee for removal purposes.
+*/
 
 function employeeAssign(employee){
     $('#tableBody').append(`
@@ -42,6 +65,14 @@ function employeeAssign(employee){
     employeeInputClear();
 }   //  end employeeAssign function
 
+//-----------------------------------------------------------------------------------------------//
+//      EMPLOYEE ADD Function
+
+/*
+This function is initiated when the submit button is clicked. It pulls the input values entered
+by the user and creates a new employee object which is then added to the employees array.
+*/
+
 function employeeAdd(){
     let employeeNew = {
         firstName: $('#firstName').val(),
@@ -54,6 +85,14 @@ function employeeAdd(){
     employeeList();
 }   //  end employeeAdd function
 
+//-----------------------------------------------------------------------------------------------//
+//      EMPLOYEE INPUT CLEAR Function
+
+/*
+This function is called after the input fields are submitted. All this does is clear out the
+input fields so a duplicate is not accidently created.
+*/
+
 function employeeInputClear(){
     $('#firstName').val('');
     $('#lastName').val('');
@@ -61,6 +100,16 @@ function employeeInputClear(){
     $('#empTitle').val('');
     $('#annualSalary').val('');
 }   //  end employeeInputClear function
+
+//-----------------------------------------------------------------------------------------------//
+//      EMPLOYEE DELETE Function
+
+/*
+This function is called when a [DELETE] button is clicked. Based on the button, the associated
+table row is also removed. To keep the employees array state in line with the table data, the
+employees array is filtered to exclude any objects that contain an empID that matches the id
+attached to the same button used to delete the row.
+*/
 
 function employeeDelete(){
     $(this).closest('tr').remove();
@@ -71,6 +120,15 @@ function employeeDelete(){
     employeeList();
 }   //  end employeeDelete function
 
+//-----------------------------------------------------------------------------------------------//
+//      EMPLOYEE SALARY TOTAL Function
+
+/*
+When this function is called, the annualSalaryTotal variable is incrementally increased based on 
+employee data input. After the variable is incremented, the table footer is cleared, removing
+the old value and then append to add the most current value.
+*/
+
 function employeeSalaryTotal(employee){
     annualSalaryTotal += employee.annualSalary;
     $('#tableFoot').empty();
@@ -79,3 +137,6 @@ function employeeSalaryTotal(employee){
         <td class="td7">$${annualSalaryTotal}</td>
     </tr>`);
 }   //  end employeeSalaryTotal function
+
+//-----------------------------------------------------------------------------------------------//
+//      The End
